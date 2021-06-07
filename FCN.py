d81b1054cdcddc75aa0870c44f0a2ee4308cbbd8
haruhi-sudo <59717968+haruhi-sudo@users.noreply.github.com>
2021-06-07 20:09:37 +0800

    second

--- FCN.py
+++ FCN.py
@@ -8,6 +8,20 @@ class Sigmod:
     def backward(self, x):
         return x * (1-x) # sigmod函数的梯度可以用自身表示
 
+class Relu:
+    def forward(self, x):
+        return np.maximum(x, 0)
+
+    def backward(self, x):
+        return 1.0 * (x > 0)
+
+class Linear:
+    def forward(self, x):
+        return x
+
+    def backward(self, x):
+        return np.ones(x.shape)    
+
 class FCN:
     '''
         全连接层的一层，准确来说是两层之间的参数
@@ -15,12 +29,14 @@ class FCN:
     def __init__(self, input_size, output_size, activator):
         self.input = np.zeros((input_size, 1))
         self.output = np.zeros((output_size, 1))
+        
         # 全连接层激活函数
         self.activator = activator
-        # 全连接层权重
-        self.weights = np.random.uniform(-0.1, 0.1, (output_size, input_size))
-        # 全连接层偏置
-        self.bias =  np.zeros((output_size, 1))
+        
+        # 初始化
+        std = np.sqrt(2. / (input_size + output_size))
+        self.weights = np.random.normal(loc=0., scale=std, size=[output_size, input_size]) # 全连接层权重    
+        self.bias =  np.random.normal(loc=0., scale=std, size=[output_size, 1]) # 全连接层偏置
     
     # 前向计算
     def forward(self, input_data):
@@ -28,13 +44,13 @@ class FCN:
         self.output = np.dot(self.weights, self.input) + self.bias
         self.output = self.activator.forward(self.output)
 
-    # 反向传播更新
+    # 反向传播计算梯度
     def backward(self, delta):
         '''
             输入这一层的误差，计算梯度和下一层的误差
+            关于什么是误差delta，可参考报告
         '''
-        self.delta = self.activator.backward(self.input) * np.dot(\
-            self.weights.T, delta) # 下一层的误差
+        self.delta = self.activator.backward(self.input) * np.dot(self.weights.T, delta) # 下一层的误差
         self.d_weights =  np.dot(delta, self.input.T) # 计算weights梯度
         self.d_bias = delta # 计算bias梯度
     
@@ -49,8 +65,10 @@ class FCN:
 class NetWork:
     def __init__(self, layers):
         self.layers = []
-        for i in range(len(layers) - 1):
+        for i in range(len(layers) - 2):
             self.layers.append(FCN(layers[i], layers[i+1], Sigmod()))
+
+        self.layers.append(FCN(layers[i+1], layers[i+2], Sigmod()))
 
     def predict(self, predict_data):
         predict_result = predict_data
@@ -63,25 +81,33 @@ class NetWork:
 
     def calc_gradient(self, label):
         # 输出层的误差
-        delta = self.layers[-1].activator.backward(\
-            self.layers[-1].output) * (label - self.layers[-1].output)
+        delta = self.layers[-1].activator.backward(self.layers[-1].output) * (label - self.layers[-1].output)
         for layer in self.layers[::-1]:
             layer.backward(delta) # 将误差传递给隐藏层
             delta = layer.delta
         return delta
 
-    def train(self, one_batch_data, one_batch_label, learning_rate, epoch):
-        for i in range(epoch):
-            for j in range(len(one_batch_data)):
-                self.predict(one_batch_data[j])
-                self.calc_gradient(one_batch_label[j])
+    def train(self, one_batch_data, one_batch_label, learning_rate):
+        for j in range(len(one_batch_data)):
+            self.predict(one_batch_data[j])
+            self.calc_gradient(one_batch_label[j])
                 
-                for layer in self.layers:
-                    layer.update(learning_rate)
+            for layer in self.layers:
+                layer.update(learning_rate)
+
+    def evaluate(self, train_data, train_label):
+        test_result = []
+        for i in range(len(train_data)):
+            test_result.append([self.predict(train_data[i]), train_label[i]])
+        print()
+        return np.sum([0.5 * (x - y) ** 2 for (x, y) in test_result])
+
 
 class DataLoader:
+    '''
+        随机梯度下降算法所需的batch
+    '''
     def __init__(self, batch_size, dataset, labelset):
-        self.batch_size = batch_size
         self.batch_data = []
         self.batch_label = []
 
@@ -97,5 +123,8 @@ class DataLoader:
                 data_tmp = []
                 label_tmp = []
         
-        self.batch_data.append(data_tmp)
-        self.batch_label.append(label_tmp)
+        if len(data_tmp) != 0:
+            self.batch_data.append(data_tmp)
+        
+        if len(label_tmp) != 0:
+            self.batch_label.append(label_tmp)
--- main.py
+++ main.py
@@ -3,22 +3,45 @@ from FCN import NetWork, DataLoader
 
 train_data = []
 train_label = []
-with open('sincos.txt','r') as f:
+with open('sincos_train.txt','r') as f:
     for line in f.readlines():
         tmp = line[:-1].split(' ')
         train_data.append(np.array([[float(tmp[0])],[float(tmp[1])]]))
-        train_label.append(np.array([float(tmp[2])]))
-        print(' ')
+        train_label.append(np.array([[float(tmp[2])]]))
 
-dataLoader = DataLoader(batch_size=10, dataset=train_data, labelset=train_label)
+dataLoader = DataLoader(batch_size=100, dataset=train_data, labelset=train_label)
 train_data = dataLoader.batch_data
 train_label = dataLoader.batch_label
 
-net = NetWork([2, 300, 1])
+net = NetWork([2, 30, 1])
 
-for epoch in range(100):
+# for epoch in range(100000):
+#     loss = 0
+#     for i in range(len(train_data)):
+#         net.train(one_batch_data=train_data[i], one_batch_label=train_label[i], learning_rate=0.5)
+#         for j in range(len(train_data[i])):
+#             predict_res = net.predict(train_data[i][j])
+#             loss = loss + np.sum(predict_res - train_label[i][j])
+#     print(loss)
+epochs = 10000000
+learning_rate = 1 
+loss = 0
+
+for epoch in range(epochs):
     for i in range(len(train_data)):
-        net.train(one_batch_data=train_data[i], one_batch_label=train_label[i], learning_rate=0.1 ,epoch=1)
+        net.train(one_batch_data=train_data[i], one_batch_label=train_label[i], learning_rate=learning_rate/np.sqrt(epoch+1))
+    
+    for i in range(len(train_data)):
+        loss = loss + net.evaluate(train_data=train_data[i], train_label=train_label[i])
+
+    # if(epoch % 100 == 0):
+    #     print(loss/100)
+    #     if(loss/100 < 0.001):
+    #         break
+    #     loss = 0
+    print(loss/10)
+    loss = 0
+
 
 
 with open('predict_res.txt', 'w') as f:
